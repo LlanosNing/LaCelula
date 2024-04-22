472ef8c9cc755aa2afffbd925a7548f758061a7a
LlanosNing <llanosning@gmail.com>
2024-04-22 13:24:01 +0200

    graficos zona hierro
    plataforma 1. Falta ajustarle el fondo blanco (hay que recortarlo más)

---
+++ Assets/Code/Scripts/Cameras/CameraMovementGrass.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class CameraMovementGrass : MonoBehaviour
+{
+    public Transform targetPlayer;
+    public float minHeight, maxHeight;
+    public Transform farBackground, middleBackground;
+    private Vector2 _lastPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Al empezar el juego la última posición del jugador será la actual
+        _lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(targetPlayer.position.x, Mathf.Clamp(targetPlayer.position.y, minHeight, maxHeight), transform.position.z);
+
+        //Referencia que me permite conocer cuanto hay que moverse en X e Y
+        Vector2 _amountToMove = new Vector2(transform.position.x - _lastPos.x, transform.position.y - _lastPos.y);
+
+        //Como el fondo del cielo se mueve a la misma velocidad que el jugador, le decimos que se mueva lo mismo que este
+        //farBackground.position = farBackground.position + new Vector3(_amountToMoveX, 0f, 0f);
+        farBackground.position = farBackground.position + new Vector3(_amountToMove.x, _amountToMove.y, 0f);
+        //El fondo de las nubes se va a mover sin embargo a la mita de velocidad que lleve el jugador, luego se moverá la mitad
+        //middleBackground.position += new Vector3(_amountToMoveX * .5f, 0f, 0f);
+        middleBackground.position += new Vector3(_amountToMove.x, _amountToMove.y, 0f) * .5f;
+
+        //Actualizamos la posición del jugador
+        //_lastXPos = transform.position.x;
+        _lastPos = transform.position;
+    }
+}
+
--- Assets/Code/Scripts/LevelMechanics/MovingPlatform.cs
+++ Assets/Code/Scripts/LevelMechanics/MovingPlatform.cs
@@ -19,15 +19,12 @@ public class MovingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Movemos la plataforma hasta el punto actual al que queremos ir, a una velocidad dada
         _platformPosition.position = Vector3.MoveTowards(_platformPosition.position, points[currentPoint].position, moveSpeed * Time.deltaTime);
 
         //Si la plataforma prácticamente ha llegado a su punto de destino
         if (Vector3.Distance(_platformPosition.position, points[currentPoint].position) < 0.01f)
         {
-            //Pasamos al siguiente punto
             currentPoint++;
-
             //Comprobamos si hemos llegado al último punto del array
             if (currentPoint >= points.Length)
                 //Reseteamos al primer punto del array
