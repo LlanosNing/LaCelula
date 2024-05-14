9351f4c02a604d2d80ce177e3c93419ac1a481b3
LlanosNing <llanosning@gmail.com>
2024-05-14 11:59:12 +0200

    enemigos
    cuando matas un enemigo te da un punto de ADN. (solo esta implementado en los rojos creo xd, hay que revisarlo)

--- Assets/Code/Scripts/Enemies/EnemyDeath.cs
+++ Assets/Code/Scripts/Enemies/EnemyDeath.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
+    private LevelManager _lMReference;
+    private UIController _uIReference;
+
+    private void Start()
+    {
+        _lMReference = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        _uIReference = GameObject.Find("Canvas").GetComponent<UIController>();
+    }
     public void EnemyDeathController()
     {
         AudioManager.audioMReference.PlaySFX(7);
         transform.gameObject.SetActive(false);
+        _lMReference.gemCollected++;
+        _uIReference.UpdateGemCount();
     }
 }
